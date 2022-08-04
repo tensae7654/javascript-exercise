3cd0fa937b69394d5b744fb1b3de5baca0828cc4
Tensae <tensaeb2016@gmail.com>
2022-08-04 10:44:41 +0300

    updated

---
+++ question 16.js
@@ -0,0 +1,26 @@
+
+function check(){
+    var number1= Number(document.getElementById("num1").value);
+    var number2= Number(document.getElementById("num2").value);
+    var sum=number1+number2;
+    
+
+   if(number1==50){
+
+    console.log(true);
+
+   }else if(number2==50)
+   {
+    console.log(true);
+
+   }else if(sum==50){
+    console.log(true);
+
+   }else{
+
+    console.log(false);
+   }
+
+
+
+}
