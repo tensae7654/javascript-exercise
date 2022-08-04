da51b5a00fd0b3b6cd8a3de4da3a177233ff8b09
Tensae <tensaeb2016@gmail.com>
2022-08-04 10:25:28 +0300

    updated

--- multdiv.js
+++ multdiv.js
@@ -1,14 +1,17 @@
 
-var number1= document.getElementById("num1").value;
-var number2= document.getElementById("num2").value;
+
 
 function multiply(){
-
-    console.log(number1*number2);
+    var number1= document.getElementById("num1").value;
+var number2= document.getElementById("num2").value;
+   var mult= number1*number2;
+    console.log(mult);
 }
 
 
 function divide(){
-
-    console.log(number1/number2);
+    var number1= document.getElementById("num1").value;
+var number2= document.getElementById("num2").value;
+    var div= number1/number2;
+    console.log(div);
 }
