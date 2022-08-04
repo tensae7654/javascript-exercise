21facf83aa07bfcce98579a1d772f8cd9c5effba
Tensae <tensaeb2016@gmail.com>
2022-08-04 11:17:52 +0300

    updated

---
+++ question 150.js
@@ -0,0 +1,17 @@
+//Write a JavaScript program that accept two integers and display the larger.
+function large(){
+    var number1= Number(document.getElementById("num1").value);
+    var number2= Number(document.getElementById("num2").value);
+
+    if(number1> number2 )
+    {
+        console.log(number1)
+
+    }else{
+
+        console.log(number2)
+    }
+
+
+
+}
