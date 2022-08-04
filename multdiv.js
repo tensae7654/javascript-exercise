b4135a49c35d06c7cf7510c9d311a3d193d8c488
Tensae <tensaeb2016@gmail.com>
2022-08-04 10:21:49 +0300

    multiply and divide

---
+++ multdiv.js
@@ -0,0 +1,14 @@
+
+var number1= document.getElementById("num1").value;
+var number2= document.getElementById("num2").value;
+
+function multiply(){
+
+    console.log(number1*number2);
+}
+
+
+function divide(){
+
+    console.log(number1/number2);
+}
